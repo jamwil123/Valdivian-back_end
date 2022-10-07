695a5e84d56b97720ca8766334096ab49faf72dd
JackenO <mboy@hotmail.co.uk>
2022-10-07 17:02:34 +0100

    added seeding info and data

--- db/seed-db.ts
+++ db/seed-db.ts
@@ -1,11 +1,16 @@
 import {MongoClient} from "mongodb";
-import {books} from "./data";
+import {products} from "./product-data";
+import { users } from "./users-data";
+import { reviews } from "./reviews-data";
 import dotenv from 'dotenv';
 dotenv.config({ path: `${__dirname}/../.env` })
 
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASSWORD;
 const dbUri = `mongodb+srv://${dbUser}:${dbPass}@valdivian.v5qmnu1.mongodb.net/?retryWrites=true&w=majority`;
+
+// current collections
+// Products / Reviews / Users
 
 async function seed() {
     const client = new MongoClient(dbUri);
@@ -17,11 +22,38 @@ async function seed() {
         await client.connect();
         console.log("connected")
 
+        await emptyCollection(
+            client,
+            "Products"
+        );
+        await emptyCollection(
+            client,
+            "Reviews"
+        );
+        await emptyCollection(
+            client,
+            "Users"
+        );
+
         // make DB calls
         await createListing(
             client,
-            books
+            "Products",
+            products
         );
+
+        await createListing(
+            client,
+            "Users",
+            users
+        );
+
+        await createListing(
+            client,
+            "Reviews",
+            reviews
+        );
+
     } catch {
         console.error
     } finally {
@@ -32,13 +64,21 @@ async function seed() {
 
 seed();
 
-async function createListing(client:any, newListing:object) {
+async function emptyCollection(client:any, collName:string) {
     const result = await client
         .db("Valdivian-db")
-        .collection("Products")
+        .collection(collName)
+        .deleteMany({});
+    console.log(result)
+}
+
+async function createListing(client:any, collName:string, newListing:object) {
+    const result = await client
+        .db("Valdivian-db")
+        .collection(collName)
         .insertMany(newListing);
     console.log(
         `${result.insertedCount} new listing(s) created with the following id(s):`
     );
-    console.log(result.insertedIds);
+    console.log(result.insertedIds);  
 }
